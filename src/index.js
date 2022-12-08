fa37c10d4040ea689daf38df4158cee5d35459de
Bruno <brunobsribeiro@gmail.com>
2022-12-08 20:17:48 -0300

    fix: modified uuid local

--- src/index.js
+++ src/index.js
@@ -7,14 +7,19 @@ app.use(express.json())
 
 const customers = [];
 
-
-
-
 app.post('/account', (req, res) => {
   const {cpf, name} = req.body;
-  const id = uuidv4();
+
+  const customersAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if(customersAlreadyExists) {
+    return res.status(400).json({error: "Customer already exists!"});
+  }
+
   customers.push({
-    id,
+    id: uuidv4(),
     cpf,
     name,
     statement: [],
