8d860623a66ebac50bf632f59e6a3cad8c508202
Bruno <brunobsribeiro@gmail.com>
2022-12-08 21:36:45 -0300

    fix : fix a bug

--- src/index.js
+++ src/index.js
@@ -1,31 +1,38 @@
 const express = require('express');
-const {v4: uuidv4} = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 const customers = [];
 
 app.post('/account', (req, res) => {
-  const {cpf, name} = req.body;
+  const { cpf, name } = req.body;
 
   const customersAlreadyExists = customers.some(
     (customer) => customer.cpf === cpf
   );
 
-  if(customersAlreadyExists) {
-    return res.status(400).json({error: "Customer already exists!"});
+  if (customersAlreadyExists) {
+    return res.status(400).json({ error: 'Customer already exists!' });
   }
 
   customers.push({
-    id: uuidv4(),
     cpf,
     name,
+    id: uuidv4(),
     statement: [],
   });
   return res.status(201).send();
-})
+});
 
+app.get('/statement/:cpf', (req, res) => {
+  const { cpf } = req.params;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  return res.json(customer.statement);
+});
 
 app.listen(3333);
